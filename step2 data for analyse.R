a81b8808c34c06261c42414a3bb390e1c5043b79
gbjx <42103897+gbjx@users.noreply.github.com>
2022-09-30 10:33:54 +0800

    Update step2 data for analyse.R

--- step2 data for analyse.R
+++ step2 data for analyse.R
@@ -4,12 +4,57 @@ getwd()
 library(dplyr)
 library(tidyr)
 load('./script/liver.Rdata')
+table(seer$`Derived AJCC N, 7th ed (2010-2015)`)
+table(seer$`Derived AJCC M, 7th ed (2010-2015)`)
+
+seer$ajcc8<-ifelse(seer$`Derived AJCC N, 7th ed (2010-2015)`=="N1","IV","A")
+seer$ajcc8<-ifelse(seer$`Derived AJCC M, 7th ed (2010-2015)`==" M1","IV",seer$ajcc8)
+table(seer$`CS tumor size (2004-2015)`)
+table(seer$nodules)
+table( seer$vascular_invasion)
+
+table(seer$ajcc8)
+
+seer$ajcc8<-ifelse(seer$`CS tumor size (2004-2015)`<=20 & seer$nodules=="one" & 
+                     seer$`Derived AJCC N, 7th ed (2010-2015)`!="N1","I",seer$ajcc8)
+
+seer$ajcc8<-ifelse(seer$`CS tumor size (2004-2015)`>20 & seer$nodules=="one" & seer$vascular_invasion=="no" & 
+                     seer$`Derived AJCC N, 7th ed (2010-2015)`!="N1","I",seer$ajcc8)
+
+seer$ajcc8<-ifelse(seer$`CS tumor size (2004-2015)`>20 & seer$nodules=="one" & seer$vascular_invasion=="yes" & 
+                     seer$`Derived AJCC N, 7th ed (2010-2015)`!="N1","II",seer$ajcc8)
+
+seer$ajcc8<-ifelse(seer$`CS tumor size (2004-2015)`<50 & seer$nodules=="more"  & 
+                     seer$`Derived AJCC N, 7th ed (2010-2015)`!="N1","II",seer$ajcc8)
+
+seer$ajcc8<-ifelse(seer$`CS tumor size (2004-2015)`>=50 & seer$nodules=="more"  & 
+                     seer$`Derived AJCC N, 7th ed (2010-2015)`!="N1","III",seer$ajcc8)
+
+table(seer$vascular_invasion)
+table(seer$`Derived AJCC M, 7th ed (2010-2015)`)
+
+seer$bclc<-ifelse(seer$nodules=="one" & seer$vascular_invasion=="no" & seer$`Derived AJCC M, 7th ed (2010-2015)`=="M0",
+                  "I","A")
+table(seer$bclc)
+
+seer$bclc<-ifelse(seer$nodules=="more" & seer$`CS tumor size (2004-2015)`<=30 & 
+                    seer$vascular_invasion=="no" & seer$`Derived AJCC M, 7th ed (2010-2015)`=="M0","I",seer$bclc)
+
+table(seer$bclc) 
+
+seer$bclc<-ifelse(seer$nodules=="more" & seer$`CS tumor size (2004-2015)`>30 & 
+                    seer$vascular_invasion=="no" & seer$`Derived AJCC M, 7th ed (2010-2015)`=="M0","II",seer$bclc)
+table(seer$bclc) 
+
+seer$bclc<-ifelse(seer$vascular_invasion=="yes" | seer$`Derived AJCC M, 7th ed (2010-2015)`=="M1","III",seer$bclc)
+table(seer$bclc) 
+
 seer<-select(seer,-1)
 plot(table(seer$`Year of diagnosis`)) 
 
 ##
 ##{2010 2011 2012 2013 2014 2015 
-##185  192  183  207  208  236 }
+##202  206  203  223  228  263 }
 
 seer<-select(seer,-1)
 
@@ -116,7 +161,7 @@ seer$lobe<-factor(seer$lobe,levels = c("one","more"),labels = c(1,2),ordered = T
 colnames(seer)
 names(seer)<-c("garde","tumor_size","lymph_nodes","surging","surg_rad","Chemotherapy","Systemic_treatment","maritalstatus",
                "Race","Age","Stage","T","N","M","label","Survival_months","number_of_tumors","nodules",
-               "lobe","vascular_invasion","AFP","Fibrosis_score")
+               "lobe","vascular_invasion","AFP","Fibrosis_score","ajcc8","bclc")
 
                        
 table(seer$Stage)
@@ -132,22 +177,29 @@ seer$Stage<-ifelse(seer$Stage %in% c("IVA","IVB"),"IV",seer$Stage)
 table(seer$Stage)
 seer$Stage<-factor(seer$Stage,levels = c("I","II","III","IV"),labels = c(1:4),ordered = TRUE)
 
+table(seer$ajcc8)
+seer$ajcc8<-factor(seer$ajcc8,levels = c("I","II","III","IV"),labels = c(1:4),ordered = TRUE)
+
+table(seer$bclc)
+seer$bclc<-factor(seer$bclc,levels = c("I","II","III"),labels = c(1:3),ordered = TRUE)
+
 seer_autoscor<-seer
 class(seer_autoscor)
 str(seer_autoscor)
 
 
-seer_autoscor<-select(seer_autoscor,c(1:14,16:22),"label")
+seer_autoscor<-select(seer_autoscor,c(1:14,16:24),"label")
 
 
 
 
-for(i in names(seer_autoscor)[1:21]){seer_autoscor[,i]<-as.numeric(seer_autoscor[,i])}
+for(i in names(seer_autoscor)[1:23]){seer_autoscor[,i]<-as.numeric(seer_autoscor[,i])}
 str(seer_autoscor)
 seer_autoscor$label<-as.factor(seer_autoscor$label)
 str(seer_autoscor)
 save(seer_autoscor,file = './script/seer_autoscor.Rdata')
 
+#三线表
 rm(list = ls())
 load('./script/seer_autoscor.Rdata')
 library(tableone)
@@ -156,15 +208,19 @@ dput(names(seer_autoscor))
 myvars<-c("garde", "tumor_size", "lymph_nodes", "surging", "surg_rad", 
           "Chemotherapy", "Systemic_treatment", "maritalstatus", "Race", 
           "Age", "Stage", "T", "N", "M", "Survival_months", "number_of_tumors", 
-          "nodules", "lobe", "vascular_invasion", "AFP", "Fibrosis_score", 
+          "nodules", "lobe", "vascular_invasion", "AFP", "Fibrosis_score","ajcc8", "bclc", 
           "label")
 
 ## 需要转为分类变量的变量
 catVars <- c("garde",  "lymph_nodes", "surging", "surg_rad", 
              "Chemotherapy", "Systemic_treatment", "maritalstatus", "Race", 
               "Stage", "T", "N", "M", "number_of_tumors",  
-             "nodules", "lobe", "vascular_invasion", "AFP", "Fibrosis_score", 
+             "nodules", "lobe", "vascular_invasion", "AFP", "Fibrosis_score", "ajcc8", "bclc",
              "label")
+summary(seer_autoscor)
+#library(modelsummary)
+
+
 tab <- CreateTableOne(vars = myvars, data = seer_autoscor, factorVars = catVars)
 print(tab, showAllLevels = TRUE)
 
